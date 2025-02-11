62dc06461822d5a1fc13a323d96aa8268c2cc11e
Umesh <umeshtamang129@gmail.com>
2025-02-11 22:51:16 +0545

    Project Setup with database

---
+++ library/bookservice_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BookserviceAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bookservice_app'
---
+++ library/notificationservice_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NotificationserviceAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'notificationservice_app'
