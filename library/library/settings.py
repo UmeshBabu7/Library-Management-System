86ac617d07240b274b6c2921d3b7c0b9d24a193c
Umesh <umeshtamang129@gmail.com>
2025-02-12 08:53:10 +0545

    create a member model and customize admin

--- library/library/settings.py
+++ library/library/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'bookservice_app',
+    'memberservice_app',
     
 ]
 
--- library/memberservice_app/models.py
+++ library/memberservice_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    membership_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
