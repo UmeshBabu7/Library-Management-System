4e519823e51c8dbd0c3d8369c10ecda01e662e30
Umesh <umeshtamang129@gmail.com>
2025-02-12 09:14:54 +0545

    create modelviewset with crud operations and urls for borrowingservice_app

--- library/borrowingservice_app/urls.py
+++ library/borrowingservice_app/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BorrowingViewSet
+
+router = DefaultRouter()
+router.register(r'borrowings', BorrowingViewSet)
 
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
--- library/library/urls.py
+++ library/library/urls.py
@@ -5,5 +5,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('book/', include('bookservice_app.urls')),
-    path('member/',include('memberservice_app.urls'))
+    path('member/',include('memberservice_app.urls')),
+    path('borrowing/',include('borrowingservice_app.urls'))
 ]
