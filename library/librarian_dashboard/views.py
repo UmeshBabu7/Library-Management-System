86ec2d282d94eaf8d2b2f0bd1867609f3e83e70b
Umesh <umeshtamang129@gmail.com>
2025-02-12 09:56:30 +0545

    create a modelviewset and urls for librarian dashboard

--- library/librarian_dashboard/views.py
+++ library/librarian_dashboard/views.py
@@ -1,3 +1,33 @@
+from rest_framework import viewsets
+from datetime import date
+from .models import Book, Member, Borrowing
+from .serializers import BookSerializer, MemberSerializer, BorrowingSerializer
 from django.shortcuts import render
+from django.db.models import Count
 
-# Create your views here.
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+def librarian_dashboard(request):
+    total_books = Book.objects.count()
+    total_members = Member.objects.count()
+    books_borrowed = Borrowing.objects.filter(return_date__isnull=True).count()
+    overdue_books = Borrowing.objects.filter(return_date__isnull=True, due_date__lt=date.today()).count()
+    popular_books = Book.objects.annotate(borrow_count=Count('borrowing')).order_by('-borrow_count')[:5]
+    context = {
+        'total_books': total_books,
+        'total_members': total_members,
+        'books_borrowed': books_borrowed,
+        'overdue_books': overdue_books,
+        'popular_books': popular_books,
+    }
+    return render(request, 'librarian_dashboard.html', context)
--- library/library/urls.py
+++ library/library/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('book/', include('bookservice_app.urls')),
     path('member/',include('memberservice_app.urls')),
-    path('borrowing/',include('borrowingservice_app.urls'))
+    path('borrowing/',include('borrowingservice_app.urls')),
+    path('librarian/',include('librarian_dashboard.urls'), name='librarian_dashboard'),
 ]
